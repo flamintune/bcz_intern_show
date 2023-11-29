e41ce4a4e13c1f361bc63eb9c42528c3cbb3d865
flamintune <bcz-intern-front-02@baicizhan.com>
2023-11-29 14:42:56 +0900

    feat: ✨ 添加时间条 加上动画取消

--- src/page/Bcz/AvatarAnimation/AvatarAnimation.jsx
+++ src/page/Bcz/AvatarAnimation/AvatarAnimation.jsx
@@ -9,6 +9,8 @@ export default function AvatarAnimation() {
   const firstImageRef = useRef(null)
   const lastImageRef = useRef(null)
   const [visiable, setVisiable] = useState(false)
+  const [duration, setDuration] = useState(50)
+
   // 打字机动画
   useLayoutEffect(() => {
     let width = titleRef.current.clientWidth
@@ -50,7 +52,6 @@ export default function AvatarAnimation() {
       maskRef.current.style.width = borderRef.current.clientWidth + 'px'
       maskRef.current.style.height = borderRef.current.clientHeight + 'px'
       maskRef.current.style.background = 'black'
-
       // todo fisrt compute first postion
       // todo last
       // todo invert
@@ -62,9 +63,9 @@ export default function AvatarAnimation() {
       const scale = firstImageBox.width / lastImageBox.width
       lastImageRef.current.style.transformOrigin = 'left top'
       //   lastImageRef.current.style.transform = `translate(${deltaX}px,${deltaY}px) scale(${scale})`
-      animate(
+      let runtime = animate(
         {
-          duration: 500,
+          duration: duration,
         },
         { translateX: deltaX, translateY: deltaY, scale: scale, borderRadius: 100 },
         { translateX: 0, translateY: 0, scale: 1, borderRadius: 0 },
@@ -73,6 +74,7 @@ export default function AvatarAnimation() {
           lastImageRef.current.style.transform = `translate(${currentState.translateX}px,${currentState.translateY}px) scale(${currentState.scale})`
         },
       )
+      return ()=>{runtime.cancel()}
     } else {
       borderRef.current.style.overflowY = 'scroll'
     }
@@ -82,44 +84,59 @@ export default function AvatarAnimation() {
       <div className={styles.title} ref={titleRef}>
         AvatarAnimation
       </div>
-      点击头像查看动画，滑动页面，再次点击查看
-      <div ref={borderRef} className={styles.phoneBorder}>
-        <img
-          ref={firstImageRef}
-          alt="avatar"
-          className={styles.avatar}
-          src="https://vol-v6.bczcdn.com/group/avatar/default0.png"
-          onClick={() => {
-            console.log('clicked')
-            setVisiable(true)
-          }}
-        />
-        <div style={{ height: 650 }}></div>
-        {visiable && (
-          <div
-            className="Mask"
-            ref={maskRef}
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              border: '16px solid #FFF',
-              borderRadius: 36,
+      <div style={{ marginBottom: 20 }}>点击头像查看动画，滑动页面，再次点击查看</div>
+      <div style={{display:'flex',flexDirection:'row-reverse'}}>
+        <div style={{marginLeft:20}}>
+          <input
+            type="range"
+            min="100"
+            max="1000"
+            value={duration}
+            onChange={e => {
+              setDuration(e.target.value)
             }}
+            defaultValue={duration}
+          />
+          <p>当前动画速度：{duration}ms</p>
+        </div>
+        <div ref={borderRef} className={styles.phoneBorder}>
+          <img
+            ref={firstImageRef}
+            alt="avatar"
+            className={styles.avatar}
+            src="https://vol-v6.bczcdn.com/group/avatar/default0.png"
             onClick={() => {
-              setVisiable(false)
+              console.log('clicked')
+              setVisiable(true)
             }}
-          >
-            <img
-              ref={lastImageRef}
-              alt="avatar"
-              style={{ width: '100%' }}
-              src="https://vol-v6.bczcdn.com/group/avatar/default0.png"
-              onClick={() => {
-                setVisiable(true)
+          />
+          <div style={{ height: 650 }}></div>
+          {visiable && (
+            <div
+              className="Mask"
+              ref={maskRef}
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                border: '16px solid #FFF',
+                borderRadius: 36,
               }}
-            />
-          </div>
-        )}
+              onClick={() => {
+                setVisiable(false)
+              }}
+            >
+              <img
+                ref={lastImageRef}
+                alt="avatar"
+                style={{ width: '100%' }}
+                src="https://vol-v6.bczcdn.com/group/avatar/default0.png"
+                onClick={() => {
+                  setVisiable(true)
+                }}
+              />
+            </div>
+          )}
+        </div>
       </div>
     </>
   )
