5c93f83d793ac428de9515d68e685405cd5b00e2
flamintune <1853995253@qq.com>
2023-12-06 03:00:05 +0900

    fix: 🐛 重构样式

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React, {  lazy } from 'react';
+import React, { lazy, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 const BCZ = lazy(() => import('./page/Bcz/Bcz.jsx'));
 function App() {
+  useEffect(() => { document.title = "实习作品集" }, [])
   return (
     <div className="App">
       <header className="App-header">
         <Router>
           <Routes>
-              <Route path="/bcz/*" element={<BCZ></BCZ>} />
+            <Route path="/bcz/*" element={<BCZ></BCZ>} />
           </Routes>
         </Router>
       </header>
